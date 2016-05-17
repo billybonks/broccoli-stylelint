d383546246536ed5c66e6b555e768b1dedf864a5
billybonks <sebastienstettler@gmail.com>
2016-05-17 17:12:52 +0800

    Add test generation functionality

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,11 @@
 var StyleLinter = require('..');
 var broccoli = require('broccoli');
 var chai = require('chai');
+var chaiAsPromised = require("chai-as-promised");
 var fs = require('fs');
+var walkSync = require('walk-sync');
 
+chai.use(chaiAsPromised);
 var assert = chai.assert;
 var expect = chai.expect;
 
@@ -76,6 +79,28 @@ describe('Configuration', function() {
   })
 });
 
+describe('Generated Tests', function(){
+  it('Correctly handles nested folders', function() {
+    var options = {disableConsoleLogging:true, generateTests:true, linterConfig:{syntax:'sass', formatter: 'string'}};
+    var treePromise = buildAndLint('tests/fixtures/test-generation',options)
+    .then(function(results){
+      var outputPath = results.directory;
+      results = walkSync(outputPath, ['tests/*.js'])
+      return results;
+    })
+    return assert.eventually.deepEqual(treePromise, [ 'tests/has-errors.scss.test.js', 'tests/has-errors2.scss.test.js' ])
+  })
+
+  it('doesnt generate tests when option set to false', function() {
+    var treePromise = buildAndLint('tests/fixtures/test-generation')
+    .then(function(results){
+      var outputPath = results.directory;
+      results = walkSync(outputPath, ['tests/*.js'])
+      return results.length;
+    })
+    return assert.eventually.equal(treePromise,0)
+  })
+})
 
 function buildAndLint(sourcePath, options) {
  var options = options || {disableConsoleLogging:true, linterConfig:{syntax:'sass', formatter: 'string'}};
