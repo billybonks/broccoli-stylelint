f88ff22fdc883b040d44cb885cca4d641aef3d8b
billybonks <sebastienstettler@gmail.com>
2016-05-18 14:16:48 +0800

    Overhaul code
    jshint
    Config
    add new tests/ refactor old ones
    rename stylelintrc

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 var Filter = require('broccoli-filter');
 var stylelint = require('stylelint');
-var merge = require('merge')
+var merge = require('merge');
 var path       = require('path');
 var fs         = require('fs');
 var escapeErrorString = require('js-string-escape');
 
 StyleLinter.prototype = Object.create(Filter.prototype);
-StyleLinter.prototype.constructor = StyleLinter
+StyleLinter.prototype.constructor = StyleLinter;
+StyleLinter.prototype.availableOptions = ['onError', 'generateTests', 'linterConfig', 'disableConsoleLogging'];
 
 /**
  * Creates a new StyleLinter instance.
  * Options
- * - linterConfig (StyleLint options)
+ * - linterConfig           (StyleLint options)
+ * - onError                (Hook when error occurs)
+ * - generateTests          (Hook when error occurs)
+ * - disableConsoleLogging  (Disables error logging in console)
  * @class
  */
+
+ //test config file
 function StyleLinter(inputNodes, options) {
-  this.options = options || {};
-  this.setSyntax(options.linterConfig.syntax);
-  this.onError = options.onError;
-  this.generateTests = options.generateTests;
-  this.disableConsoleLogging = options.disableConsoleLogging;
+  this.options = options || {linterConfig:{}};
+
+  for(var i = 0; i < this.availableOptions.length; i++){
+    var option = this.availableOptions[i];
+    this[option] = options[option];
+    delete options[option];
+  }
 
   merge({
-    configFile: process.cwd()+'/.stylelintrc.json',
-    formatter: 'string',
-    syntax: 'scss'
-  }, options.linterConfig)
+    configFile: '/.stylelintrc',
+    syntax: 'scss',
+    formatter: 'string'
+  }, this.linterConfig);
 
-  /* Set passed options */
-
-  for (var key in options) {
-    if (options.hasOwnProperty(key)) {
-      this[key] = options[key]
-    }
+  if(this.linterConfig.formatter !== 'verbose' || this.linterConfig.formatter !== 'string'){
+      this.linterConfig.formatter = 'string';
   }
+
+  this.linterConfig.files = null;
+
+  this.setSyntax(this.linterConfig.syntax);
+
   Filter.call(this, inputNodes, options);
 }
 
@@ -45,23 +54,24 @@ StyleLinter.prototype.setSyntax = function(syntax) {
   var extensions = [];
   var targetExtension;
   if(!syntax)
-    syntax = 'css'
+    syntax = 'scss';
+    this.linterConfig.syntax = syntax;
   if(syntax === 'scss' || syntax === 'sass') {
-    if(syntax == 'scss'){
-      extensions.push('sass')
+    if(syntax === 'scss'){
+      extensions.push('sass');
     } else {
-      extensions.push('scss')
+      extensions.push('scss');
     }
     targetExtension = syntax;
   } else if(syntax === 'sugarss') {
-    targetExtension = 'sss'
+    targetExtension = 'sss';
   } else {
-    targetExtension = syntax
+    targetExtension = syntax;
   }
   extensions.push(targetExtension);
   this.extensions = extensions;
   this.targetExtension = targetExtension;
-}
+};
 
 /** Filter Class Overrides **/
 
@@ -71,31 +81,30 @@ StyleLinter.prototype.setSyntax = function(syntax) {
  */
 StyleLinter.prototype.build = function() {
   return Filter.prototype.build.call(this).finally(function() {
-  })
-}
+  });
+};
 
 /**
  * Entry point for broccoli build
  * @override
  */
 StyleLinter.prototype.processString = function(content, relativePath) {
-  _this = this;
+  var _this = this;
   this.linterConfig.code = content;
   return stylelint.lint(this.linterConfig).then(function(results){
     if(results.errored){
       if(_this.onError)
-        _this.onError(results)
+        _this.onError(results);
       if(_this.generateTests)
-        _this.testGenerator(relativePath,results.output)
+        _this.testGenerator(relativePath,results.output);
       if(!_this.disableConsoleLogging )
-        console.log(results.output)
+        console.log(results.output);
     }
   })
   .catch(function(err) {
     // do things with err e.g.
     console.error(err.stack);
   });
-  return content;
 };
 
 /**
@@ -111,8 +120,8 @@ StyleLinter.prototype.testGenerator = function(relativePath, errors) {
              "test('" + relativePath + " should pass style-lint', function() {\n" +
              "  ok(" + false + ", '" + relativePath + " should pass style-lint." + escapeErrorString('\n' + errors) + "');\n" +
              "});\n";
-  var fileName = relativePath.split(path.sep)
-  fileName = fileName[fileName.length - 1]
+  var fileName = relativePath.split(path.sep);
+  fileName = fileName[fileName.length - 1];
   var directory = this.outputPath + path.sep + 'tests';
   var testPath = directory + path.sep + fileName +'.test.js';
   if (!fs.existsSync(directory)){
--- tests/test.js
+++ tests/test.js
@@ -2,110 +2,154 @@ var StyleLinter = require('..');
 var broccoli = require('broccoli');
 var chai = require('chai');
 var chaiAsPromised = require("chai-as-promised");
-var fs = require('fs');
 var walkSync = require('walk-sync');
 
 chai.use(chaiAsPromised);
 var assert = chai.assert;
 var expect = chai.expect;
+var builder, lintErrors;
 
-describe('Broccoli build', function() {
+describe('Broccoli StyleLint Plugin', function() {
+
+
+  afterEach(function() {
+    if (builder.cleanup) {
+      builder.cleanup();
+    }
+  });
 
   beforeEach(function() {
     lintErrors = [];
   });
 
-  afterEach(function() {
-    if (builder) {
-      builder.cleanup();
-    }
+  describe('Broccoli build', function() {
+
+    it('catches errors', function() {
+      return buildAndLint('tests/fixtures/has-errors').then(function(results) {
+        assert.equal(lintErrors[0].results[0].warnings.length,2);
+      });
+    });
+
+    it('ignores file specified config');
+
   });
 
-  it('catches errors', function() {
-    return buildAndLint('tests/fixtures/has-errors').then(function(results) {
-      assert.equal(lintErrors[0].results[0].warnings.length,2)
+  describe('Configuration', function() {
+
+    describe('Syntax', function(){
+
+      function assertExtensions(syntax, targetExtension, extensions){
+        var options = {linterConfig:{syntax:syntax}};
+        builder = new StyleLinter('', options);
+        expect(builder.targetExtension).to.equal(targetExtension);
+        expect(builder.extensions).to.eql(extensions);
+      }
+
+      it('accepts less',function(){
+        var extension = 'less';
+        assertExtensions(extension, extension, [extension]);
+      });
+
+      it('accepts sass',function(){
+        var extension = 'sass';
+        assertExtensions(extension, extension, ['scss', extension]);
+      });
+
+      it('accepts scss',function(){
+        var extension = 'scss';
+        assertExtensions(extension, extension, ['sass', extension]);
+      });
+
+      it('accepts sugarss',function(){
+        var extension = 'sss';
+        assertExtensions('sugarss', extension, [extension]);
+      });
+
+      it('accepts css',function(){
+        var extension = 'css';
+        assertExtensions(extension, extension, [extension]);
+      });
+
+      it('defaults to scss',function(){
+        var extension = 'scss';
+        assertExtensions(null, extension, ['sass', extension]);
+      });
+
+    });
+
+    describe('StyleLint Configuration', function(){
+
+      it('cant override formatter', function(){
+        var options = {linterConfig:{formatter:'verbose'}};
+        var tree = new StyleLinter('', options);
+        assert.equal(tree.linterConfig.formatter, 'string');
+      });
+
+      it('cant set files option', function(){
+        var options = {linterConfig:{files:['a','b']}};
+        var tree = new StyleLinter('', options);
+        assert.equal(tree.linterConfig.files, null);
+
+      });
+
+    });
+
+    it('sets options on object', function(){
+      var linterConfig = {syntax:'sass'};
+      var options = {linterConfig:linterConfig, generateTests:true};
+      var linter = new StyleLinter('', options);
+      expect(linter.generateTests).to.equal(true);
+      expect(linter.linterConfig).to.eql(linterConfig);
+    });
+
+    describe('Tests', function () {
+
+      it('doesnt generateTests when option is false', function() {
+        return expect(buildAndLint('tests/fixtures/test-generation')
+                              .then(walkTestsOutputTree)
+                              .then(function(result){return result.length;})
+              ).to.eventually.equal(0);
+      });
+    });
+
+  });
+
+  describe('Generated Tests', function(){
+    var generateTestsConfig;
+
+    beforeEach(function() {
+      generateTestsConfig  = {disableConsoleLogging:true, generateTests:true, linterConfig:{syntax:'sass', formatter: 'string'}};
+    });
+
+    it('correctly handles nested folders', function() {
+      return expect(buildAndLint('tests/fixtures/test-generation', generateTestsConfig)
+                              .then(walkTestsOutputTree))
+             .to.eventually.eql([ 'tests/has-errors.scss.test.js', 'tests/has-errors2.scss.test.js' ]);
+    });
+
+    it('doesnt generate tests when no there is no error', function() {
+      return expect(buildAndLint('tests/fixtures/no-errors', generateTestsConfig)
+                            .then(walkTestsOutputTree)
+                            .then(function(result){return result.length;})
+             ).to.eventually.equal(0);
+    });
+
+    it('generates correct qunit test string', function(){
+
     });
   });
 });
 
-describe('Configuration', function() {
-  describe('Syntax', function(){
+function walkTestsOutputTree(results){
+    var outputPath = results.directory;
+    results = walkSync(outputPath, ['tests/*.js']);
+    return results;
+}
 
-    function assertExtensions(syntax, targetExtension, extensions){
-      var options = {linterConfig:{syntax:syntax}};
-      var linter = new StyleLinter('', options);
-      expect(linter.targetExtension).to.equal(targetExtension);
-      expect(linter.extensions).to.eql(extensions)
-    }
-
-    it('accepts less',function(){
-      var extension = 'less'
-      var linter = assertExtensions(extension, extension, [extension])
-    })
-
-    it('accepts sass',function(){
-      var extension = 'sass'
-      var linter = assertExtensions(extension, extension, ['scss', extension]);
-    })
-
-    it('accepts scss',function(){
-      var extension = 'scss'
-      var linter = assertExtensions(extension, extension, ['sass', extension]);
-    })
-
-    it('accepts sugarss',function(){
-      var extension = 'sss'
-      var linter = assertExtensions('sugarss', extension, [extension]);
-    })
-
-    it('accepts css',function(){
-      var extension = 'css'
-      var linter = assertExtensions(extension, extension, [extension])
-    })
-
-    it('defaults to css',function(){
-      var extension = 'css'
-      var linter = assertExtensions(null, extension, [extension])
-    })
-
-  })
-
-  it('sets options on object', function(){
-    var linterConfig = {syntax:'sass'}
-    var options = {linterConfig:linterConfig, generateTests:true};
-    var linter = new StyleLinter('', options);
-    expect(linter.generateTests).to.equal(true);
-    expect(linter.linterConfig).to.eql(linterConfig);
-  })
-});
-
-describe('Generated Tests', function(){
-  it('Correctly handles nested folders', function() {
-    var options = {disableConsoleLogging:true, generateTests:true, linterConfig:{syntax:'sass', formatter: 'string'}};
-    var treePromise = buildAndLint('tests/fixtures/test-generation',options)
-    .then(function(results){
-      var outputPath = results.directory;
-      results = walkSync(outputPath, ['tests/*.js'])
-      return results;
-    })
-    return assert.eventually.deepEqual(treePromise, [ 'tests/has-errors.scss.test.js', 'tests/has-errors2.scss.test.js' ])
-  })
-
-  it('doesnt generate tests when option set to false', function() {
-    var treePromise = buildAndLint('tests/fixtures/test-generation')
-    .then(function(results){
-      var outputPath = results.directory;
-      results = walkSync(outputPath, ['tests/*.js'])
-      return results.length;
-    })
-    return assert.eventually.equal(treePromise,0)
-  })
-})
-
-function buildAndLint(sourcePath, options) {
- var options = options || {disableConsoleLogging:true, linterConfig:{syntax:'sass', formatter: 'string'}};
-  options.onError =function(results) {
-    lintErrors.push(results)
+function buildAndLint(sourcePath, options, onError) {
+  options = options || {disableConsoleLogging:true, linterConfig:{syntax:'sass', formatter: 'string'}};
+  options.onError = function(results) {
+    lintErrors.push(results);
   };
 
   var tree = new StyleLinter(sourcePath, options);
