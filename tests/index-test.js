7ad775cda0740f84570e366f5ad7ed10d02d0029
billybonks <sebastienstettler@gmail.com>
2018-03-13 07:31:50 +0800

    add factory method

---
+++ tests/index-test.js
@@ -0,0 +1,12 @@
+const BroccoliStylelint = require('../index');
+
+describe('broccoli-stylelint', () => {
+  describe('constructors', () => {
+    describe('#create', () => {
+      test('It returns a broccoli-styleliny instance', () => {
+        let linter = BroccoliStylelint.create('', {});
+        expect(linter.constructor).toEqual(BroccoliStylelint);
+      });
+    });
+  });
+});
