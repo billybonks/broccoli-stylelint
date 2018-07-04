2a3ebbc9e5a3108e5704cb8486ef7f416727ae7b
billybonks <sebastienstettler@gmail.com>
2018-07-05 00:31:04 +0800

    [TEST] Update

--- tests/test-generator-test.js
+++ tests/test-generator-test.js
@@ -1,44 +1,4 @@
 'use strict';
-
-/* eslint-disable no-useless-escape*/
-let outputs ={
-  qunit: {
-    errored:
-`QUnit.module('Stylelint');
-QUnit.test('has-errors.scss should pass stylelint', function(assert) {
-  assert.expect(1);
-  assert.ok(false, '1:15 Unexpected empty block (block-no-empty)\\n6:10 Expected \\\\\"#000000\\\\\" to be \\\\\"black\\\\\" (color-named)');
-});
-`,
-  passed:
-`QUnit.module('Stylelint');
-QUnit.test('no-errors.scss should pass stylelint', function(assert) {
-  assert.expect(1);
-  assert.ok(true, 'no-errors.scss should pass stylelint');
-});
-`
-},
-  mocha: {
-    errored:
-`describe('Stylelint', function() {
-  it('has-errors.scss should pass stylelint', function() {
-    // test failed
-    var error = new chai.AssertionError('1:15 Unexpected empty block (block-no-empty)\\n6:10 Expected \\\\\"#000000\\\\\" to be \\\\\"black\\\\\" (color-named)');
-    error.stack = undefined;
-    throw error;
-  });
-});
-`,
-    passed:
-`describe('Stylelint', function() {
-  it('no-errors.scss should pass stylelint', function() {
-    // test passed
-  });
-});
-`
-  }
-};
-
 const frameworks = ['qunit', 'mocha'];
 const generator = require('../lib/test-generator');
 const errors = require('./fixtures/errors');
@@ -50,11 +10,11 @@ describe('test-generator', function() {
       describe(framework, function() {
         it('generates correct errored test', function(){
           let results = generator('has-errors.scss', errors, framework);
-          expect(results).toEqual(outputs[framework].errored);
+          expect(results).toMatchSnapshot();
         });
         it('generates correct passed test', function(){
           let results = generator('no-errors.scss', noErrors, framework);
-          expect(results).toEqual(outputs[framework].passed);
+          expect(results).toMatchSnapshot();
         });
       });
     });
