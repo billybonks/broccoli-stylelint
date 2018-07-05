c8ad1e6fe25f7de882fecac2b3437cba330fd549
billybonks <sebastienstettler@gmail.com>
2018-07-05 22:12:37 +0800

    [REFACTOR] Add suite generator

--- tests/test-generator-test.js
+++ tests/test-generator-test.js
@@ -1,20 +1,24 @@
 'use strict';
 const frameworks = ['qunit', 'mocha'];
-const generator = require('../lib/test-generator');
+const generators = ['test-generator', 'suite-generator'];
+
 const errors = require('./fixtures/errors');
 const noErrors = require('./fixtures/no-errors');
 
-describe('test-generator', function() {
-  describe('errors is defined', function() {
-    frameworks.forEach(function(framework){
-      describe(framework, function() {
-        it('generates correct errored test', function(){
-          let results = generator('has-errors.scss', errors, framework);
-          expect(results).toMatchSnapshot();
-        });
-        it('generates correct passed test', function(){
-          let results = generator('no-errors.scss', noErrors, framework);
-          expect(results).toMatchSnapshot();
+generators.forEach(function(generator){
+  describe(generator, function() {
+    let _generator = require('../lib/test-generator');
+    describe('errors is defined', function() {
+      frameworks.forEach(function(framework){
+        describe(framework, function() {
+          it('generates correct errored test', function(){
+            let results = _generator('has-errors.scss', errors, framework);
+            expect(results).toMatchSnapshot();
+          });
+          it('generates correct passed test', function(){
+            let results = _generator('no-errors.scss', noErrors, framework);
+            expect(results).toMatchSnapshot();
+          });
         });
       });
     });
